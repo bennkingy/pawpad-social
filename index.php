c5974f802d5f3b695e57afff97760a2d1bd3b9bb
Benn King <bennkingy@gmail.com>
2019-02-10 18:01:38 +0000

    Code tidy ups

--- index.php
+++ index.php
@@ -3,8 +3,11 @@
 
   echo "<br><span class='main'>Welcome to $appname,";
 
-  if ($loggedin) echo " $user, you are logged in.";
-  else           echo ' please sign up and/or log in to join in.';
+  if ($loggedin) {
+      echo " $user, you are logged in.";
+  } else {
+      echo ' please sign up and/or log in to join in.';
+  }
 ?>
 
     </span><br><br>
